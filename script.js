f401e67b28e501f345c168650bd8f3007ee3a539
Hardik Singh <hardiksingh297@gmail.com>
2020-08-22 18:25:37 +0530

    Added scroll on reveal

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ $(document).ready(function () {
     arrows: false,
   });
 });
+
+//SCROLL REVEAL
+const invitees = document.querySelectorAll(".invitee");
+const sponsors = document.querySelectorAll(".sponsor");
+const config = {
+  reset: true,
+  duration: 2000,
+};
+ScrollReveal().reveal(invitees, config);
+ScrollReveal().reveal(sponsors, config);
