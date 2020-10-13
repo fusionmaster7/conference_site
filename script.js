93e0a952f45505dedb556b0025c5996a9aa5d462
mangesh <mshanit@gmail.com>
2020-10-13 13:23:34 +0530

    updated header text and added editors pannel

---
+++ script.js
@@ -0,0 +1,19 @@
+if(document.getElementById('countdown')){
+    let destination= new Date("Nov 6, 2020 11:00:00").getTime();
+                
+    setInterval(function(){
+        let now= new Date().getTime();
+
+        let timeRemaining= destination - now;
+
+        let days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+        document.getElementById('countdown').innerHTML= '<span class="start-text">STARTS IN :</span>'+'<b style="color:white;">'+days+'</b>'+'<span class="timer-text">days : </span>'+'<b style="color:white;">'+hours+'</b>'+'<span class="timer-text">hours : </span>'+'<b style="color:white;">'+minutes+'</b>'+'<span class="timer-text">minutes : </span>'+'<b style="color:white;">'+seconds+'</b>'+'<span class="timer-text">seconds</span>';
+        if(timeRemaining<0){
+            document.getElementById('countdown').innerHTML=' ';
+        }
+    },1000)
+}
