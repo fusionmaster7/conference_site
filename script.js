60f52358618bb1022bc40532227260b3357f0d0e
Hardik Singh <hardiksingh297@gmail.com>
2020-10-14 09:52:33 +0530

    Added About section and pages

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ if(document.getElementById('countdown')){
         let minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
         let seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
-        document.getElementById('countdown').innerHTML= '<span class="start-text">STARTS IN :</span>'+'<b style="color:white;">'+days+'</b>'+'<span class="timer-text">days : </span>'+'<b style="color:white;">'+hours+'</b>'+'<span class="timer-text">hours : </span>'+'<b style="color:white;">'+minutes+'</b>'+'<span class="timer-text">minutes : </span>'+'<b style="color:white;">'+seconds+'</b>'+'<span class="timer-text">seconds</span>';
+        document.getElementById('countdown').innerHTML= '<span class="start-text">STARTS IN :</span>'+'<b style="color:white;"> '+days+' </b>'+'<span class="timer-text">days : </span>'+'<b style="color:white;"> '+hours+' </b>'+'<span class="timer-text">hours : </span>'+'<b style="color:white;"> '+minutes+' </b>'+'<span class="timer-text">minutes : </span>'+'<b style="color:white;"> '+seconds+' </b>'+'<span class="timer-text">seconds</span>';
         if(timeRemaining<0){
             document.getElementById('countdown').innerHTML=' ';
         }
